089ce0c5dba65af0a2054a29cbae48ab92f7a30c
John Kelvie <john.kelvie@gmail.com>
2017-09-15 23:57:49 -0500

    Added source to client

--- src/SkillBotClient.ts
+++ src/SkillBotClient.ts
@@ -3,9 +3,10 @@ import * as request from "request-promise-native";
 export class SkillBotClient {
     public constructor(public baseURL: string = "https://skillbot.io") {}
 
-    public message(userID: string, utterance: string): Promise<any> {
+    public message(source: string, userID: string, utterance: string): Promise<any> {
         const url = this.baseURL + "/message" +
-            "?userID=" + userID +
+            "?source=" + source +
+            "&userID=" + userID +
             "&utterance=" + utterance;
 
         const options = {
